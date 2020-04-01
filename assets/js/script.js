e3f0a0e0d14e0de0a781aa099ca1c8e8b72326df
Elijah <elijaherap3@gmail.com>
2020-04-01 14:46:14 -0700

    changed event listener to react to entire form

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,12 @@
-var buttonEl = document.querySelector("#save-task");
+var formEl = document.querySelector("#task-form");
 var tasksToDoEl = document.querySelector("#tasks-to-do");
 
-var createTaskHndler = function(){
+var createTaskHandler = function(){
+    event.preventDefault();
     var listItemEl = document.createElement("li");
     listItemEl.className = "task-item";
     listItemEl.textContent = "This is a new task.";
     tasksToDoEl.appendChild(listItemEl);
 }
 
-buttonEl.addEventListener("click", createTaskHndler);
+formEl.addEventListener("submit", createTaskHandler);
